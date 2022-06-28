5aeb4f63dc9a5b57c0a78fe6907cd25d56762f52
Alexandru-Gurau <alexandru.gurau@live.com>
2022-06-28 16:09:32 +0300

    updating directory page with directory component and item component

--- src/index.js
+++ src/index.js
@@ -9,12 +9,16 @@ import reportWebVitals from "./reportWebVitals";
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>,
-//   document.getElementById('root')
+//   document.getElementById("root")
 // );
 
 // REACT 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
