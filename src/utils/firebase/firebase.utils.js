c1cba76244d11aa5e8b215873c9831663179767a
Alexandru-Gurau <alexandru.gurau@live.com>
2022-07-15 11:31:14 +0300

    update firebase utils

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk",
-  authDomain: "crwn-clothing-db-98d4d.firebaseapp.com",
-  projectId: "crwn-clothing-db-98d4d",
-  storageBucket: "crwn-clothing-db-98d4d.appspot.com",
-  messagingSenderId: "626766232035",
-  appId: "1:626766232035:web:506621582dab103a4d08d6",
+  apiKey: "AIzaSyCuSPrPvhK0oi9Vos5jM1SBO--8aqqyQFw",
+  authDomain: "crwn-clothing-db-1836b.firebaseapp.com",
+  projectId: "crwn-clothing-db-1836b",
+  storageBucket: "crwn-clothing-db-1836b.appspot.com",
+  messagingSenderId: "868977906083",
+  appId: "1:868977906083:web:b1b53846259c4760720a16",
 };
 
 initializeApp(firebaseConfig);
